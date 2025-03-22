6e2c3c44980d84de094648205144cbfc139b317d
Sara Marcely <saradomacario@gmail.com>
2025-03-21 22:26:00 -0300

    Aula PAM - TipCalculator

---
+++ TipCalculator/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿namespace TipCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        bool arredondarPraCima
+        bool arredondarPraBaixo
+
+        public MainPage()
+        {
+            InitializeComponent();
+            //Nesse contexto, o +=
+            billInput.TextChanged += (s, e) => CalculateTip(false, false);
+            roundDown.Clicked += (s, e) => CalculateTip(false, true);
+            roundUp.Clicked += (s, e) => CalculateTip(true, false);
+
+            tipPercentSlider.ValueChanged += (s, e) =>
+            {
+                double pct = Math.Round(e.NewValue);
+                tipPercent.Text = pct + "%";
+                CalculatorTip(false, false);
+            }
+        }
+
+        private void OnNormalTip (object sender, EventArgs e)
+        {
+           double gorjeta = Convert.ToDouble(billInput.Text);
+           double percentualDaGorjeta = TipoPercentSlider.Value;
+           double gorjeta = valor * (percentualDaGorjeta / 100);
+        }
+
+
+        private void CalculateTip()
+        {
+            double valor = Convert.ToDouble(billInput.Text);
+            double percentualDaGorjeta = tipPercentSlider.Value;
+            double gorjeta = valor * (percentualDaGorjeta / 100);
+            
+            if (arredondarPraBaixo)
+            { 
+                 Math.Celling(gorjeta);
+            }
+            if (arredondarPraCima)
+            {
+                 Math.Floor(gorjeta)
+            }
+            double total = valor + gorjeta;
+            tipOutPut.Text = total.ToString(gorjeta);
+            totalOutPut = Convert.ToString(total);
+        }
+        private void OnGenerousTip(object sender, EventArgs e)
+        {
+            tipPercentSlider.Value = 15;
+        }
+    }
