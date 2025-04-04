12d15c2375908d834a76c64b17a884a6cebaf59f
Sara Marcely <saradomacario@gmail.com>
2025-04-04 19:45:24 -0300

    PAM - TipCalculator 040425

--- TipCalculator/MainPage.xaml.cs
+++ TipCalculator/MainPage.xaml.cs
@@ -1,31 +1,24 @@
-﻿namespace TipCalculator
+﻿using Microsoft.Maui.Graphics.Text;
+using System.Reflection.Metadata.Ecma335;
+
+namespace TipCalculator
 {
     public partial class MainPage : ContentPage
     {
-        bool arredondarPraCima
-        bool arredondarPraBaixo
+        bool arrendondaPraCima;
+        bool arrendondaPraBaixo;
 
         public MainPage()
         {
             InitializeComponent();
-            //Nesse contexto, o +=
-            billInput.TextChanged += (s, e) => CalculateTip(false, false);
-            roundDown.Clicked += (s, e) => CalculateTip(false, true);
-            roundUp.Clicked += (s, e) => CalculateTip(true, false);
-
-            tipPercentSlider.ValueChanged += (s, e) =>
-            {
-                double pct = Math.Round(e.NewValue);
-                tipPercent.Text = pct + "%";
-                CalculatorTip(false, false);
-            }
+            tipPercentSlider.ValueChanged += (s, e) => CalculateTip();
         }
+
+
 
         private void OnNormalTip(object sender, EventArgs e)
         {
-            double gorjeta = Convert.ToDouble(billInput.Text);
-            double percentualDaGorjeta = TipoPercentSlider.Value;
-            double gorjeta = valor * (percentualDaGorjeta / 100);
+            tipPercentSlider.Value = 15;
         }
 
 
@@ -34,47 +27,38 @@
             double valor = Convert.ToDouble(billInput.Text);
             double percentualDaGorjeta = tipPercentSlider.Value;
             double gorjeta = valor * (percentualDaGorjeta / 100);
-
-            if (arredondarPraBaixo)
+            if (arrendondaPraCima)
             {
-                Math.Celling(gorjeta);
+                gorjeta = Math.Ceiling(gorjeta);
             }
-            if (arredondarPraCima)
+            if (arrendondaPraBaixo)
             {
-                Math.Floor(gorjeta)
+                gorjeta = Math.Floor(gorjeta);
             }
             double total = valor + gorjeta;
-            tipOutPut.Text = total.ToString(gorjeta);
-
-            totalOutPut = Convert.ToString(total);
+            //Currency
+            tipOutput.Text = gorjeta.ToString("C");
+            totalOutput.Text = total.ToString("C");
+            tipPercent.Text = tipPercentSlider.Value.ToString();
         }
+
+
         private void OnGenerousTip(object sender, EventArgs e)
         {
-            tipPercentSlider.Value = 15;
+            tipPercentSlider.Value = 20;
         }
 
-
-        void CalculateTip(bool roundUp, bool roundDown)
+        private void roundUp_Clicked(object sender, EventArgs e)
         {
-            double t;
-            if (Double.TryParse(billInput.Text, out t) && t > 0)
-            {
-                double pct = Math.Round(tipPercentSlider.Value);
-                double tip = Math.Round(t * (pct / 100.0), 2);
-
-                double final = t + tip;
-                if (roundUp)
-                {
-                    final = Math.Ceiling(final);
-                    tip = final - t;
-                }
-                else if (roundDown)
-                {
-                    final = Math.floor(final);
-                    tip = final - t;
-                }
-
-                tipOutput.Text = tip.ToString("C");
-                totalOutput.Text = final.ToString("C");
-            }
+            arrendondaPraCima = true;
+            arrendondaPraBaixo = false;
         }
+
+        private void roundDown_Clicked(object sender, EventArgs e)
+        {
+            arrendondaPraCima = false;
+            arrendondaPraBaixo = true;
+        }
+    }
+
+}
