a6be30d7e2fea425c293f5d10a0ab9fd652a2ff0
Sara Marcely <saradomacario@gmail.com>
2025-04-04 19:34:10 -0300

    PAM - TipCalculator 280325

--- TipCalculator/MainPage.xaml.cs
+++ TipCalculator/MainPage.xaml.cs
@@ -21,11 +21,11 @@
             }
         }
 
-        private void OnNormalTip (object sender, EventArgs e)
+        private void OnNormalTip(object sender, EventArgs e)
         {
-           double gorjeta = Convert.ToDouble(billInput.Text);
-           double percentualDaGorjeta = TipoPercentSlider.Value;
-           double gorjeta = valor * (percentualDaGorjeta / 100);
+            double gorjeta = Convert.ToDouble(billInput.Text);
+            double percentualDaGorjeta = TipoPercentSlider.Value;
+            double gorjeta = valor * (percentualDaGorjeta / 100);
         }
 
 
@@ -34,21 +34,47 @@
             double valor = Convert.ToDouble(billInput.Text);
             double percentualDaGorjeta = tipPercentSlider.Value;
             double gorjeta = valor * (percentualDaGorjeta / 100);
-            
+
             if (arredondarPraBaixo)
-            { 
-                 Math.Celling(gorjeta);
+            {
+                Math.Celling(gorjeta);
             }
             if (arredondarPraCima)
             {
-                 Math.Floor(gorjeta)
+                Math.Floor(gorjeta)
             }
             double total = valor + gorjeta;
             tipOutPut.Text = total.ToString(gorjeta);
+
             totalOutPut = Convert.ToString(total);
         }
         private void OnGenerousTip(object sender, EventArgs e)
         {
             tipPercentSlider.Value = 15;
         }
-    }
+
+
+        void CalculateTip(bool roundUp, bool roundDown)
+        {
+            double t;
+            if (Double.TryParse(billInput.Text, out t) && t > 0)
+            {
+                double pct = Math.Round(tipPercentSlider.Value);
+                double tip = Math.Round(t * (pct / 100.0), 2);
+
+                double final = t + tip;
+                if (roundUp)
+                {
+                    final = Math.Ceiling(final);
+                    tip = final - t;
+                }
+                else if (roundDown)
+                {
+                    final = Math.floor(final);
+                    tip = final - t;
+                }
+
+                tipOutput.Text = tip.ToString("C");
+                totalOutput.Text = final.ToString("C");
+            }
+        }
